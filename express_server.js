2ff429008f0be1809009dd968a5ff9914fc803de
Liz Fosdick <lizfosdick@gmail.com>
2023-02-21 18:51:57 -0800

    Added editing/updating functionality and buttons

--- express_server.js
+++ express_server.js
@@ -63,6 +63,12 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 })
 
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.longURL;
+  res.redirect("/urls");
+})
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
